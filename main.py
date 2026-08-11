acd0ce6931fbfc5de53625d1427ed975de9f20ca
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 20:55:52 +0530

    dragon 🐉

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from modules import *
 
 app = Client(
     "DRAGON-USERBOT",
-    pi_id=int(os.getenv("API_ID")),
+    api_id=int(os.getenv("API_ID")),
     api_hash=os.getenv("API_HASH"),
     session_string=os.getenv("SESSION_STRING")
 )
