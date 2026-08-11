1e07e18cd20353b1336e1c48dbcb90c0eb379b9f
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:20:42 +0530

    docs: clarify

--- modules/alive.py
+++ modules/alive.py
@@ -1,10 +1,12 @@
 import time
-from pyrogram import Client,filters
-
 ST=time.time()
 
-async def setup(c):c.on_message(filters.command("alive",prefixes=".")&filters.me)(h)
+async def setup(c):
+ from pyrogram import filters
+ c.on_message(filters.command("alive",prefixes=".")&filters.me)(h)
 
 async def h(c,m):
- u=time.time()-ST;h,rem=divmod(u,3600);mn,s=divmod(rem,60)
- await m.edit(f"**Alive!**\nUptime: `{int(h)}h {int(mn)}m {int(s)}s`")
+ u=time.time()-ST
+ hr,r=divmod(u,3600)
+ mn,s=divmod(r,60)
+ await m.edit(f"**Alive!** Uptime: `{int(hr)}h {int(mn)}m {int(s)}s`")
