241bbd976d7df021a369cbfe853456776837d0d6
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 20:58:31 +0530

    DRAGON USERBOT

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
+API_ID = int(os.getenv("AI_ID"))
 API_HASH = os.getenv("API_HASH")
 STRING_SESSION = os.getenv("STRING_SESSION")
